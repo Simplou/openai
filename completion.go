89b8f469caaf71a9c3bcb9eb8315295b2900c835
gabrielluizsf <gabrielluizads123@gmail.com>
2024-05-06 16:43:41 -0300

    openai chat completion implemented

---
+++ completion.go
@@ -0,0 +1,84 @@
+package openaigo
+
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/Simplou/goxios"
+)
+
+// CompletionRequest represents the structure of the request sent to the OpenAI API.
+type CompletionRequest struct {
+	Model      string    `json:"model"`
+	Messages   []Message `json:"messages"`
+	ToolChoice string    `json:"tool_choice,omitempty"`
+	Tools      []Tool    `json:"tools,omitempty"`
+}
+
+// Message represents a message in the conversation.
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+// Tool represents a tool that can be used during the conversation.
+type Tool struct {
+	Type     string   `json:"type"`
+	Function Function `json:"function,omitempty"`
+}
+
+// Function represents a function call that can be used as a tool.
+type Function struct {
+	Name string `json:"name"`
+}
+
+// CompletionResponse represents the structure of the response received from the OpenAI API.
+type CompletionResponse struct {
+	ID      string   `json:"id"`
+	Object  string   `json:"object"`
+	Created int64    `json:"created"`
+	Model   string   `json:"model"`
+	Choices []Choice `json:"choices"`
+	Usage   Usage    `json:"usage"`
+}
+
+// Choice represents a response choice in the conversation.
+type Choice struct {
+	Index        int         `json:"index"`
+	Message      Message     `json:"message"`
+	Logprobs     interface{} `json:"logprobs,omitempty"`
+	FinishReason string      `json:"finish_reason"`
+}
+
+// Usage represents the token usage in the request and response.
+type Usage struct {
+	PromptTokens     int `json:"prompt_tokens"`
+	CompletionTokens int `json:"completion_tokens"`
+	TotalTokens      int `json:"total_tokens"`
+}
+
+func ChatCompletion(client OpenAIClient, httpClient HTTPClient, body *CompletionRequest) (*CompletionResponse, error) {
+	client.AddHeader(goxios.Header{Key: "Content-Type", Value: "application/json"})
+	b, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	options := &goxios.RequestOpts{
+		Headers: Headers(),
+		Body:    bytes.NewBuffer(b),
+	}
+	res, err := httpClient.Post(client.BaseURL()+"/chat/completions", options)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Request.Body.Close()
+	response := new(CompletionResponse)
+	if err := goxios.DecodeJSON(res.Body, response); err != nil {
+		return nil, err
+	}
+
+	if err := res.Body.Close(); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
---
+++ completion_test.go
@@ -0,0 +1,80 @@
+package openaigo
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/Simplou/goxios"
+)
+
+type MockClient struct {
+	baseUrl string
+}
+
+func (c MockClient) Context() context.Context {
+	return context.TODO()
+}
+func (c MockClient) ApiKey() string {
+	return "mock_api_key"
+}
+func (c MockClient) BaseURL() string {
+	return c.baseUrl
+}
+
+func (c MockClient) AddHeader(h goxios.Header) {}
+
+type testHTTPClient struct {
+	req *http.Request
+}
+
+func (c *testHTTPClient) Post(url string, opts *goxios.RequestOpts) (*http.Response, error) {
+	statusCode := http.StatusOK
+	body := goxios.JSON{
+		"id":    "123",
+		"model": "gpt-3.5-turbo",
+		"choices": []Choice{
+			{
+				Index:        0,
+				Message:      Message{Role: "assistant", Content: "Hi"},
+				FinishReason: "",
+			},
+		},
+	}
+	resBytes, err := body.Marshal()
+	if err != nil {
+		return nil, err
+	}
+	resReader := bytes.NewBuffer(resBytes)
+	res := &http.Response{
+		Request:       c.req,
+		Status:        http.StatusText(statusCode),
+		StatusCode:    statusCode,
+		Header:        http.Header{},
+		Body:          io.NopCloser(resReader),
+		ContentLength: int64(len(resBytes)),
+	}
+
+	return res, nil
+}
+
+func TestChatCompletionRequest(t *testing.T) {
+	mockClient := MockClient{"http://localhost:399317"}
+	httpClient := testHTTPClient{}
+	completionRequest := &CompletionRequest{
+		Model:    "gpt-3.5-turbo",
+		Messages: []Message{{Role: "user", Content: "Hello!"}},
+	}
+
+	response, err := ChatCompletion(mockClient, &httpClient, completionRequest)
+	if err != nil {
+		t.Errorf("Erro ao chamar ChatCompletion: %v", err)
+	}
+
+	expectedID := "123"
+	if response.ID != expectedID {
+		t.Errorf("ID da resposta esperado: %s, ID recebido: %s", expectedID, response.ID)
+	}
+}
