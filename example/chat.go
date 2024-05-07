8c77466d04359627c4ee3544208e0e5490c753c0
gabrielluizsf <gabrielluizads123@gmail.com>
2024-05-07 10:34:55 -0300

    audio apis implemented

--- example/chat.go
+++ example/chat.go
@@ -1,30 +1,21 @@
 package main
 
 import (
-	"context"
 	"log"
-	"os"
 
-	"github.com/Simplou/goxios"
 	openai "github.com/Simplou/openai"
 )
 
 func chat() {
-	var (
-		ctx        = context.Background()
-		apiKey     = os.Getenv("OPENAI_KEY")
-		client     = openai.New(ctx, apiKey)
-		httpClient = goxios.New(ctx)
-	)
- 	body := &openai.CompletionRequest{
+	body := &openai.CompletionRequest{
 		Model: "gpt-3.5-turbo",
 		Messages: []openai.Message{
 			{Role: "user", Content: "Hello"},
 		},
 	}
-	
+
 	res, err := openai.ChatCompletion(client, httpClient, body)
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 	log.Println(res.Choices[0].Message.Content)
--- example/main.go
+++ example/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main(){
+import (
+	"context"
+	"os"
+
+	"github.com/Simplou/goxios"
+	openai "github.com/Simplou/openai"
+)
+
+var (
+	ctx        = context.Background()
+	apiKey     = os.Getenv("OPENAI_KEY")
+	client     = openai.New(ctx, apiKey)
+	httpClient = goxios.New(ctx)
+)
+
+func main() {
 	chat()
+	tts()
+	whisper()
 }
